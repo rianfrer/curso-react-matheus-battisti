2c9d7517555a31eb8764aed0e78306e08e8df07a
Rian Silva <rianfrer@gmail.com>
2022-06-02 13:29:51 -0300

    React ícons

--- meu-projeto/src/components/layout/Footer.js
+++ meu-projeto/src/components/layout/Footer.js
@@ -1,6 +1,17 @@
+import { FaFacebook, FaInstagram, FaLinkedin, FaWhatsapp } from 'react-icons/fa'
+import styles from './Footer.module.css'
+
 function Footer() {
     return (
-        <footer>Rodapé</footer>
+        <footer>
+            <ul className={styles.social_list}>
+                <li><FaFacebook /></li>
+                <li><FaInstagram /></li>
+                <li><FaWhatsapp /></li>
+                <li><FaLinkedin/></li>
+            </ul>
+            <p>Nosso rodapé</p>
+        </footer>
     )
 }
 
